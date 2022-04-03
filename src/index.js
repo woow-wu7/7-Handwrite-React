76796b47dc611831dfa40d183b1fd2120e298e3e
hanxian韩闲 <woow.wu7@gmail.com>
2022-04-03 14:46:59 +0800

    feat: event contrast2.

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const Father = () => {
     child?.addEventListener("click", eventListener("子组件 - 原生冒泡"), false);
   }, []);
 
-  const fatherReactBubbleClick = () => console.log('父组件-react合成事件-冒泡')
+  const fatherReactBubbleClick = () => console.log('父组件-react合成事件-冒泡') // 相当于执行了 dispatchEvent，会重新模拟一遍 (  捕获和冒泡 )
   const fatherReactCaptureClick = () => console.log('父组件-react合成事件-捕获')
   const childReactBubbleClick = () => console.log('子组件-react合成事件-冒泡')
   const childReactCaptureClick = () => console.log('子组件-react合成事件-捕获')
